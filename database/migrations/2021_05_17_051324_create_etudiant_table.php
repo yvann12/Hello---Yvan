f245f7976f7252a6aea198c7c323f0820575bd65
Yvan <yvannkayo@gmail.com>
2021-06-02 13:43:44 +0100

    Initial commit

---
+++ database/migrations/2021_05_17_051324_create_etudiant_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEtudiantTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('etudiant', function (Blueprint $table) {
+            $table->id();
+            $table->String('nomComplet');
+            $table->String('matricule');
+            $table->date('dateNaissance');
+            $
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('etudiant');
+    }
+}
